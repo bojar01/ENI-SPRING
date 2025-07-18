2cc5088bb6827b6d06829c0720850753df0c9c09
Laotseu <67864504+LaoTseu0@users.noreply.github.com>
2025-07-18 10:58:06 +0200

    S3024 S3025
    # Conflicts:
    #	src/main/resources/application.yml

--- src/main/java/fr/eni/ludotheque/config/SecurityConfig.java
+++ src/main/java/fr/eni/ludotheque/config/SecurityConfig.java
@@ -6,25 +6,28 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-
-public class WebSecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((requests) -> requests
-                                .requestMatchers(HttpMethod.POST, "/api/jeux").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/jeux").authenticated()
                                 .requestMatchers(HttpMethod.GET, "/api/jeux").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/api/auth/*").permitAll()
                                 .anyRequest().permitAll()
                 )
-                .httpBasic(httpBasic -> {});
+                //.httpBasic(httpBasic -> {});
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
 
         return http.build();
     }
